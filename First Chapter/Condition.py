cd230bfc29f7ff10bb03b1646408a51570805a02
9AlexeyEmelyanov <9aleshaeboshu1@gmail.com>
2023-04-19 14:53:33 +0300

    27 task completed

--- First Chapter/Condition.py
+++ First Chapter/Condition.py
@@ -1,3 +1,4 @@
+import math
 # Упражнение 26. Чет или нечет?
 def task26():
     number = int(input("Введите число: "))
@@ -6,8 +7,21 @@ def task26():
     else:
         print(f"Число {number} - не четное")
 
+# Упражнение 27. Собачий возраст
+def task27():
+    human_age = int(input("Введите возраст в годах: "))
+    if human_age < 0:
+        print("Ошибка: возраст не может быть отрицательным.")
+    else:
+        if human_age <= 2:
+            dog_age = human_age * 10.5
+        else:
+            dog_age = 10.5 * 2 + (human_age -2) * 4
+        print("Собачий возраст: {:.1f} лет".format(dog_age))
+
 def main():
-    task26()
+    #task26()
+    task27()
 
 
 if __name__ == "__main__":
